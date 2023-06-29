09c02b2a2c7c0ee6b6d23c0b7e6759a2de8d651f
leepsyko <letho161299@gmail.com>
2023-06-29 12:06:40 +0700

    sua bai 9

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//tạo giá trị sử dụng toàn bộ bài
 let numberArray = [];
 
 // Build interface for user
@@ -5,7 +6,10 @@ let numberArray = [];
 function addInteger(number) {
   let inputNumber = number;
 
+  // chuyển string thành array bằng ","
   inputNumber = inputNumber.split(",");
+
+  // lặp array để ép về kiểu dữ liệu về number
   for (let i = 0; i < inputNumber.length; i++) {
     inputNumber[i] = +inputNumber[i];
 
@@ -15,6 +19,7 @@ function addInteger(number) {
   document.getElementById("hello").innerHTML = numberArray;
 }
 
+// Nhập value đầu vào
 function add() {
   let inputNumber = document.getElementById("input-number").value;
 
@@ -26,6 +31,7 @@ function remove() {
   removeInteger(inputNumber);
 }
 
+// xoá số khỏi chuỗi
 function removeInteger(number) {
   let inputNumber = number;
 
@@ -51,8 +57,9 @@ function removeInteger(number) {
 
 function sumInte() {
   let outputSum = 0;
-
+  // lặp mảng cộng dồn nếu thoải điều kiện
   for (let i = 0; i < numberArray.length; i++) {
+    // điều kiện số dương
     if (numberArray[i] > 0) {
       outputSum += numberArray[i];
     }
@@ -83,11 +90,12 @@ function seekMinNumber() {
   let outputMinNumber = numberArray[0];
 
   for (let i = 0; i < numberArray.length; i++) {
+    // so sánh lần lượt để chọn số nhỏ nhất
     if (numberArray[i] < outputMinNumber) {
       outputMinNumber = numberArray[i];
     }
   }
-
+  // gắn đầu ra bằng 0 nếu không có đầu vào
   if (typeof outputMinNumber === "undefined") {
     outputMinNumber = 0;
   }
@@ -100,15 +108,17 @@ function seekMinNumber() {
 // Caution: other way with no method MAth.max
 
 function seekMinPositiveNumber() {
+  // lấy giá trị lớn nhất trong mảng
   let outputMinPositiveNumber = Math.max(...numberArray);
 
   for (let i = 0; i < numberArray.length; i++) {
+    // điều kiện dương mới được so sánh
     if (numberArray[i] > 0) {
       if (numberArray[i] - outputMinPositiveNumber < 0)
         outputMinPositiveNumber = numberArray[i];
     }
   }
-
+  // trường hợp số lớn nhất âm thì trả ra không có
   if (outputMinPositiveNumber <= 0) {
     outputMinPositiveNumber = "Không có";
   }
@@ -122,17 +132,17 @@ function seekMinPositiveNumber() {
 //seeking last positive number in the array
 
 function seekLastPositiveNumber() {
-  let outputLastPositiveNumber = 0;
-
+  let outputLastPositiveNumber = null;
+  //cho chạy vòng lặp array từ cuối 
   for (let i = numberArray.length - 1; i >= 0; i--) {
+    //kiểm tra điều kiện
     if (numberArray[i] > 0) {
       outputLastPositiveNumber = numberArray[i];
       break;
     }
-
   }
-
-  if (outputLastPositiveNumber === 0) {
+  // nếu rỗng trả ra -1
+  if (outputLastPositiveNumber === null) {
     outputLastPositiveNumber = -1;
   }
 
@@ -145,16 +155,16 @@ function seekLastPositiveNumber() {
 // change position of number in array with 2 position entered
 
 function transPositionNumber() {
+  // input đầu vào. vì chỉ mục bắt đầu từ 0 nên giá trị đầu vào - 1
   let inputPosition1 = +document.getElementById("transPosition1").value - 1;
   let inputPosition2 = +document.getElementById("transPosition2").value - 1;
   let numberPosition1 = 0;
   let numberPosition2 = 0;
 
+  // gắn thuộc tính chỉ mục đầu vô cho 2 biến
   numberPosition1 = numberArray[inputPosition1];
   numberPosition2 = numberArray[inputPosition2];
-  console.log(numberPosition1);
-  console.log(numberPosition2);
-
+  // Điều kiện để 2 position được điền 
   if (
     typeof numberPosition1 !== "undefined" &&
     typeof numberPosition2 !== "undefined"
@@ -171,6 +181,7 @@ function transPositionNumber() {
 // Arrangement number in small to big
 
 function arrangeNumber() {
+  // hàm để xác định cho phương thức sort
   function compare(a, b) {
     return a - b;
   }
@@ -182,47 +193,59 @@ function arrangeNumber() {
 // initial integer
 
 function firstInteger() {
-  let outputInteger = 0;
+  let outputInteger = null;
   for (let i = 0; i < numberArray.length; i++) {
+    // integer là số chia hết cho 1 và khác số 0
     if (numberArray[i] % 1 === 0 && numberArray[i] !== 0) {
       outputInteger = numberArray[i];
       break;
     }
-    console.log("g");
-  }
-  if (outputInteger === 0) {
+  } 
+  // không có trả về -1
+  if (outputInteger === null) {
     outputInteger = -1;
   }
   document.getElementById("output-firstInteger").style.background = "#EAECF0";
   document.getElementById("output-firstInteger").innerHTML = outputInteger;
 }
 
-// Enter array inti initial array
+// Enter array to initial array. then count integer
 
 function enterArray() {
   let inputArray = document.getElementById("enter-array").value;
 
-  if (inputArray  != "") {
-  inputArray = inputArray.split(",");
-  numberArray.push(
-    ...inputArray.map((value, index) => {
-      return +value;
-    })
-  );
+  if (inputArray != "") {
+    // chuyển về mảng
+    inputArray = inputArray.split(",");
+    // thêm vào mảng có sẵn bằng toán tử ... . Có sử dụng map để ép kiểu dữ liệu về number
+    numberArray.push(
+      ...inputArray.map((value, index) => {
+        return +value;
+      })
+    );
+  }
+  let countInte2 = 0
+  // tạo vòng lặp đếm số nguyên 
+  for (let i = 0; i < numberArray.length; i++) {
+    if (numberArray[i] % 1 === 0) {
+      countInte2 += 1
+    }
+    
   }
 
-
   document.getElementById("output-enterArray").style.background = "#EAECF0";
-  document.getElementById("output-enterArray").innerHTML = numberArray;
-  console.log(numberArray);
+  document.getElementById("output-enterArray").innerHTML = `${numberArray} <br>
+  Có ${countInte2} số nguyên`;
 }
 
 // compare positive number and negative number
 
 function compareNumber() {
+  // tạo array lọc giá trị âm
   let negativeNumber = numberArray.filter((value, index) => {
     return value < 0;
   });
+    // tạo array lọc giá trị dương
   let positiveNumber = numberArray.filter((value, index) => {
     return value > 0;
   });
